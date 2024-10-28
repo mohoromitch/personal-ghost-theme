75d9928fa0502f8741b72597e4ea133896bfc03a
Mitchell Mohorovich <mitchell.mohorovich@unity3d.com>
2024-10-27 22:28:02 -0400

    feat: add vertical margin to callouts + shrink footer font size + make figcaption font bigger

--- assets/built/index.js
+++ assets/built/index.js
@@ -1,7 +1,2 @@
-(function () {
-	'use strict';
-
-
-
-})();
+!function(e,l){e&&!e.getElementById("livereloadscript")&&((l=e.createElement("script")).async=1,l.src="//"+(self.location.host||"localhost").split(":")[0]+":35729/livereload.js?snipver=1",l.id="livereloadscript",e.getElementsByTagName("head")[0].appendChild(l))}(self.document);
 //# sourceMappingURL=index.js.map
